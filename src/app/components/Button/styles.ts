735f4110f61ecb53d29b753ba5186db99d321d57
mathpimentel <mathcamposp@gmail.com>
2025-10-04 23:16:13 -0300

    feat: component Button

---
+++ src/app/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#2C43B1",
+        height: 48,
+        width: "100%",
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    title: {
+        fontWeight: 600,
+        color: "#FFF",
+        fontSize: 16
+    }
+})
