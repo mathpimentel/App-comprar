b820f3dda5d4fd458784bf593bb6e4d45aa0f31e
mathpimentel <mathcamposp@gmail.com>
2025-10-05 18:16:42 -0300

    chore: restructure project folders

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,9 @@ import { styles } from "./styles"
 
 export function Input({...rest} : TextInputProps){
     return (
-        <TextInput {...rest} style={styles.container} />
+        <TextInput {...rest} style={styles.container}
+        placeholderTextColor="#74798B"
+        />
     )
 
 }
